55132a8e087f1a4c65e97a067fd6e21e03549259
Estefrank07 <estebanmoya446@gmail.com>
2023-07-06 09:43:39 -0500

    Agregar archivos de proyecto.

---
+++ emoyaS6/ActEliminar.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace emoyaS6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActEliminar : ContentPage
+    {
+        private string Url = "http://172.29.192.1/Prueba/post.php";
+        public ActEliminar(estudiante datos)
+        {
+            InitializeComponent();
+            txtCodigo.Text= datos.codigo.ToString();   
+            txtNombre.Text= datos.nombre.ToString();
+            txtApellido.Text= datos.apellido.ToString();
+            txtEdad.Text=datos.edad.ToString();
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            WebClient cliente = new WebClient();
+            var datos = new System.Collections.Specialized.NameValueCollection();
+            try
+            {
+                datos.Add("codigo", txtCodigo.Text);
+                datos.Add("nombre", txtNombre.Text);
+                datos.Add("apellido", txtApellido.Text);
+                datos.Add("edad", txtEdad.Text);
+
+                cliente.UploadValues(Url + "?codigo=" + txtCodigo.Text + "&nombre=" + txtNombre.Text + "&apellido=" + txtApellido.Text + "&edad=" + txtEdad.Text, "PUT", datos);
+              
+                Navigation.PushAsync(new MainPage());
+               
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+                
+            }
+             
+
+
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            WebClient cliente = new WebClient();
+            var datos = new System.Collections.Specialized.NameValueCollection();
+            try
+            {
+
+                datos.Add("codigo", txtCodigo.Text);
+                cliente.UploadValues(Url + "?codigo=" + txtCodigo.Text, "DELETE", datos);
+                Navigation.PushAsync(new MainPage());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ALERTA", ex.Message, "Cerrar");
+
+            }
+        }
+    }
+}
---
+++ emoyaS6/Insertar.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace emoyaS6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Insertar : ContentPage
+    {
+        public Insertar()
+           
+        {
+            InitializeComponent();
+        }
+      
+
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://172.29.192.1/Prueba/post.php", "POST", parametros);
+                await DisplayAlert("Alerta", "Dato Ingresado correctamente", "Cerrar");
+            }
+            catch (Exception ex) {
+                await DisplayAlert("alerta","Error" + ex.Message, "ok"); 
+            }
+        }
+        
+        
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
